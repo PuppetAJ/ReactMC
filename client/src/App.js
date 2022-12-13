38234415085dccefa581dc73ec9fb82a859118c2
p5eudoDjnn <alexander.havers@gmail.com>
2022-12-13 13:02:08 -0500

    set up skybox for r3b

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+// import logo from './logo.svg';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+	return (
+		<>
+			{/* <div>Outside Canvas</div> */}
+			<Canvas>
+				<Sky
+					distance={450000}
+					sunPosition={[5, 1, 8]}
+					inclination={0}
+					azimuth={0.25}
+					{...props}
+				/>
+			</Canvas>
+		</>
+	);
 }
 
 export default App;
