fa75c622d931f132ed80a37944cc593b8010b024
Caleb <calebfunderburk@icloud.com>
2022-12-12 20:58:11 -0500

    successfully connecting to Mongo server

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,10 @@ mongoose.set('debug', true);
 mongoose.set('strictQuery', true);
 
 // Settings removed due to updated mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reactmc', {});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/reactmc', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Export connection
 module.exports = mongoose.connection;
