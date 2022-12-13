bebba6195b96f154f45b510d977a4191d5ed1e4e
Dane Cronin <danecronin@gmail.com>
2022-12-13 16:00:22 -0700

    updating to recent files

--- server/config/connection.js
+++ server/config/connection.js
@@ -9,8 +9,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/reactmc', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useCreateIndex: true,
+    // useFindAndModify: false
 });
 
 // Export connection
