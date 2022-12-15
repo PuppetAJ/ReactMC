49f62d51e1c4ee0a9ebea20008550ce3bc6f5d53
Dane Cronin <danecronin@gmail.com>
2022-12-14 17:34:13 -0700

    footer styling

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -2,27 +2,27 @@ import * as React from 'react'
 import { ButtonGroup, Container, IconButton, Stack, Text } from '@chakra-ui/react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
-
 const Footer = () => {
 
   //Return JSX
   return (
 
-
+    <div className="footer-container">
   <Container
   as="footer"
   role="contentinfo"
   py={{
-    base: '12',
-    md: '16',
+    base: '2',
+    md: '4',
   }}
 >
   <Stack
     spacing={{
-      base: '4',
-      md: '5',
+      base: '2',
+      md: '2',
     }}
   >
+  
     <Stack justify="space-between" direction="row" align="center">
       
       <ButtonGroup variant="ghost">
@@ -32,7 +32,11 @@ const Footer = () => {
           aria-label="LinkedIn"
           icon={<FaLinkedin fontSize="1.25rem" />}
         />
-        <IconButton as="a" href="https://github.com/PuppetAJ/ReactMC" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
+        <IconButton as="a" href="https://github.com/PuppetAJ/ReactMC"
+         aria-label="GitHub" 
+         target="_blank"
+         rel="noreferer"
+         icon={<FaGithub fontSize="1.25rem" />} />
         <IconButton
           as="a"
           href="#"
@@ -41,11 +45,14 @@ const Footer = () => {
         />
       </ButtonGroup>
     </Stack>
-    <Text fontSize="sm" color="subtle">
+    <Text fontSize="sm" color="subtle" align="center">
       &copy; {new Date().getFullYear()} Team NotHavinIt.
     </Text>
   </Stack>
 </Container>
+</div>
+
+
 )
 };
 
