bf5e35831c8a797697a092afc7269cd52c6d291d
Dane Cronin <danecronin@gmail.com>
2022-12-15 13:11:55 -0700

    fixed nav links in NavBar Component

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -11,8 +11,8 @@ const Header = () => {
 	return (
 
 		// this needs to be re-styled to show login/signup form //
-		<header className="bg-secondary mb-4 py-2 flex-row align-center">
-			<div className="container flex-row justify-space-between-lg justify-center align-center">
+		<header >
+			<div>
 				<Link to='/'>
 					{/* <h1>website</h1> */}
 				</Link>
@@ -27,9 +27,9 @@ const Header = () => {
 						</>
 					) : (
 						<>
-						{/* Padding for these buttons needs to be changed */}
-							<Link to='/login' className="btn-minecraft">Login</Link>
-							<Link to='/signup' className="btn-minecraft">Signup</Link>
+						{/* Padding for these buttons needs to be changed These are buttons that display on landing page */}
+							<Link to='/login' className="btn-minecraft" >Login</Link>
+							<Link to='/signup' className-="btn-minecraft">Signup</Link>
 						</>
 					)}
 				</nav>
--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -47,7 +47,7 @@ const Profile = () => {
   }
 
   return (
-    <div id="profile">
+    <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
