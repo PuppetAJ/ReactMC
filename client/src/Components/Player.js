f880df5ffbe661767e5c60ae960eb26fa1237046
p5eudoDjnn <alexander.havers@gmail.com>
2022-12-15 19:10:26 -0500

    ruined playground that will never be pushed into develop and only used for testing purpose

--- client/src/Components/Player.js
+++ client/src/Components/Player.js
@@ -1,69 +1,90 @@
-import * as THREE from "three"
-import * as RAPIER from "@dimforge/rapier3d-compat"
-import { useRef } from "react"
-import { useThree, useFrame } from "@react-three/fiber"
-import { useKeyboardControls } from "@react-three/drei"
-import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier"
-import Axe from "./Axe"
+import * as THREE from "three";
+import * as RAPIER from "@dimforge/rapier3d-compat";
+import { useRef } from "react";
+import { useThree, useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
+import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier";
+import Axe from "./Axe";
 
-const SPEED = 5
-const direction = new THREE.Vector3()
-const frontVector = new THREE.Vector3()
-const sideVector = new THREE.Vector3()
-const rotation = new THREE.Vector3()
+const SPEED = 5;
+const direction = new THREE.Vector3();
+const frontVector = new THREE.Vector3();
+const sideVector = new THREE.Vector3();
+const rotation = new THREE.Vector3();
 
 export function Player({ lerp = THREE.MathUtils.lerp }) {
-  const axe = useRef()
-  const ref = useRef()
-  const rapier = useRapier()
-  const { camera } = useThree()
-  const [, get] = useKeyboardControls()
-  console.log(camera);
-  useFrame((state) => {
-    const { forward, backward, left, right, jump } = get()
-    const velocity = ref.current.linvel()
-    // update camera
-    camera.position.set(...ref.current.translation())
-    // update axe
-    axe.current.children[0].rotation.x = lerp(axe.current.children[0].rotation.x, Math.sin((velocity.length() > 1) * state.clock.elapsedTime * 10) / 6, 0.1)
-    axe.current.rotation.copy(camera.rotation)
-    axe.current.position.copy(camera.position).add(camera.getWorldDirection(rotation).multiplyScalar(1))
-    // movement
-    frontVector.set(0, 0, backward - forward)
-    sideVector.set(left - right, 0, 0)
+	const axe = useRef();
+	const ref = useRef();
+	const rapier = useRapier();
+	const { camera } = useThree();
+	const [, get] = useKeyboardControls();
+	console.log(camera);
+	useFrame((state) => {
+		const { forward, backward, left, right, jump } = get();
+		const velocity = ref.current.linvel();
+		// update camera
+		camera.position.set(...ref.current.translation());
+		// update axe
+		axe.current.children[0].rotation.x = lerp(
+			axe.current.children[0].rotation.x,
+			Math.sin((velocity.length() > 1) * state.clock.elapsedTime * 10) / 6,
+			0.1,
+		);
+		axe.current.rotation.copy(camera.rotation);
+		axe.current.position
+			.copy(camera.position)
+			.add(camera.getWorldDirection(rotation).multiplyScalar(1));
+		// movement
+		frontVector.set(0, 0, backward - forward);
+		sideVector.set(left - right, 0, 0);
 
-    // This is the code that's breaking the movement !!!!!!!!
-    // looking down or up causes you to stop moving
-    // https://threejs.org/docs/#api/en/cameras/Camera
-    // console.log(camera.rotation);
-    const {_x, _y, _z} = camera.rotation;
-    // console.log(camera.rotation);
-    const cameraRot = new THREE.Euler(_x, _y, _z);
+		// This is the code that's breaking the movement !!!!!!!!
+		// looking down or up causes you to stop moving
+		// https://threejs.org/docs/#api/en/cameras/Camera
+		// console.log(camera.rotation);
+		const { _x, _y, _z } = camera.rotation;
+		// console.log(camera.rotation);
+		const cameraRot = new THREE.Euler(_x, _y, _z);
 
-    // console.log(_x, _y, _z);
-    // console.log(frontVector);
-    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED)
-    .applyEuler(camera.rotation);
-    // console.log(direction);
-    // console.log(direction.z);
-    // console.log(direction.x);
-    // console.log(direction.z);
-    // console.log(direction.z);
-    ref.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z })
-    // jumping
-    const world = rapier.world.raw()
-    const ray = world.castRay(new RAPIER.Ray(ref.current.translation(), { x: 0, y: -1, z: 0 }))
-    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.75
-    if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 })
-  })
-  return (
-    <>
-      <RigidBody ref={ref} colliders={false} mass={1} type="dynamic" position={[0, 10, 0]} enabledRotations={[false, false, false]}>
-        <CapsuleCollider args={[0.75, 0.5]} />
-      </RigidBody>
-      <group ref={axe} onPointerMissed={(e) => (axe.current.children[0].rotation.x = -0.5)}>
-        <Axe position={[0.3, -0.35, 0.5]} />
-      </group>
-    </>
-  )
+		// console.log(_x, _y, _z);
+		// console.log(frontVector);
+		direction
+			.subVectors(frontVector, sideVector)
+			.normalize()
+			.multiplyScalar(SPEED)
+			.applyEuler(camera.rotation);
+		// console.log(direction);
+		// console.log(direction.z);
+		// console.log(direction.x);
+		// console.log(direction.z);
+		// console.log(direction.z);
+		ref.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
+		// jumping
+		const world = rapier.world.raw();
+		const ray = world.castRay(
+			new RAPIER.Ray(ref.current.translation(), { x: 0, y: -1, z: 0 }),
+		);
+		const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.75;
+		if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 });
+	});
+	return (
+		<>
+			<RigidBody
+				ref={ref}
+				colliders={false}
+				mass={1}
+				type="dynamic"
+				position={[0, 10, 0]}
+				enabledRotations={[false, false, false]}
+			>
+				<CapsuleCollider args={[0.67, 0.5]} />
+			</RigidBody>
+			<group
+				ref={axe}
+				onPointerMissed={(e) => (axe.current.children[0].rotation.x = -0.5)}
+			>
+				<Axe position={[0.3, -0.35, 0.5]} />
+			</group>
+		</>
+	);
 }
--- client/src/Pages/Editor.js
+++ client/src/Pages/Editor.js
@@ -2,42 +2,44 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Sky, PointerLockControls, KeyboardControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
-import { Cube, Cubes } from "../Components/Cube";
-import { Ground } from "../Components/Ground"
-import { Player } from "../Components/Player"
+// import Cube from "../Components/Cube";
+// import Cubes from "../Components/Cube";
+import { Cubes } from "../Components/Cubes";
+import { Cube } from "../Components/Cubes";
+import { Ground } from "../Components/Ground";
+import { Player } from "../Components/Player";
 import { Ground2 } from "../Components/Ground2";
 
 export default function Editor() {
+	// Add basic block building
+	// Find way to stitch textures together..?
+	// https://github.com/pmndrs/react-three-fiber/discussions/761
+	// https://codesandbox.io/s/vkgi6?file=/src/Player.js:179-198
+	// Bring in various block textures
 
-  // Add basic block building
-  // Find way to stitch textures together..?
-  // https://github.com/pmndrs/react-three-fiber/discussions/761
-  // https://codesandbox.io/s/vkgi6?file=/src/Player.js:179-198
-  // Bring in various block textures
-
-  return (
-    <KeyboardControls
-    map={[
-      { name: "forward", keys: ["ArrowUp", "w", "W"] },
-      { name: "backward", keys: ["ArrowDown", "s", "S"] },
-      { name: "left", keys: ["ArrowLeft", "a", "A"] },
-      { name: "right", keys: ["ArrowRight", "d", "D"] },
-      { name: "jump", keys: ["Space"] },
-    ]}>
-      <Canvas shadows camera={{fov: 45}}>
-      <Sky sunPosition={[100, 20, 100]} />
-        <ambientLight intensity={0.3} />
-        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
-        <Physics gravity={[0, -30, 0]}>
-          <Ground/>
-          <Ground2/>
-          <Player/>
-          <Cube position = {[0, 0.5, -10]}/>
-          <Cubes />
-        </Physics>
-        <PointerLockControls />
-      </Canvas>
-    </KeyboardControls>
-  )
+	return (
+		<KeyboardControls
+			map={[
+				{ name: "forward", keys: ["ArrowUp", "w", "W"] },
+				{ name: "backward", keys: ["ArrowDown", "s", "S"] },
+				{ name: "left", keys: ["ArrowLeft", "a", "A"] },
+				{ name: "right", keys: ["ArrowRight", "d", "D"] },
+				{ name: "jump", keys: ["Space"] },
+			]}
+		>
+			<Canvas shadows camera={{ fov: 45 }}>
+				<Sky sunPosition={[100, 20, 100]} />
+				<ambientLight intensity={0.3} />
+				<pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+				<Physics gravity={[0, -30, 0]}>
+					<Ground />
+					<Ground2 />
+					<Player />
+					{/* <Cube position={[0, 0.5, -10]} /> */}
+					<Cubes />
+				</Physics>
+				<PointerLockControls />
+			</Canvas>
+		</KeyboardControls>
+	);
 }
-
