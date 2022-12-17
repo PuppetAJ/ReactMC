b35780653c4caef07f640e5eb9ffe0b7f4560326
Caleb <calebfunderburk@icloud.com>
2022-12-17 18:48:47 -0500

    merging code

--- client/src/Components/ThoughtForm/index.js
+++ client/src/Components/ThoughtForm/index.js
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_THOUGHT } from '../../utils/mutations';
-import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+import React, {useState} from 'react';
+import {useMutation} from '@apollo/client';
+import {ADD_THOUGHT} from '../../utils/mutations';
+import {QUERY_THOUGHTS, QUERY_ME} from '../../utils/queries';
 
 
-const ThoughtForm = () => {
+const ThoughtForm = ()=> {
     const [thoughtText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
 
     //Declare addthought function and error variable
-    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
-        update(cache, { data: { addThought } }) {
+    const [addThought, {error}] = useMutation(ADD_THOUGHT, {
+        update(cache, {data: {addThought}}) {
             //read what's currently in the cache
             //Could potentially not exist yet so wrap in try/catch
-            try {
+            try { 
                 //update Me array's cache
-                const { me } = cache.readQuery({ query: QUERY_ME });
+                const {me} = cache.readQuery({ query: QUERY_ME});
                 cache.writeQuery({
                     query: QUERY_ME,
-                    data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+                    data: {me : {...me, thoughts: [...me.thoughts, addThought]}},
                 });
             } catch (e) {
                 console.warn('First thought insertion by user!')
             }
 
             //update thought array's cache
-            const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+            const {thoughts} = cache.readQuery({query: QUERY_THOUGHTS});
 
             //prepend the newest thought to the front of the array
             cache.writeQuery({
                 query: QUERY_THOUGHTS,
-                data: { thoughts: [addThought, ...thoughts] },
+                data: {thoughts: [addThought, ...thoughts]},
             });
         }
     });
 
-
+    
 
     const handleChange = event => {
-        if (event.target.value.length <= 280) {
+        if(event.target.value.length <= 280) {
             setText(event.target.value);
             setCharacterCount(event.target.value.length);
         }
@@ -51,7 +51,7 @@ const ThoughtForm = () => {
         try {
             //add thought to database
             await addThought({
-                variables: { thoughtText }
+                variables: {thoughtText}
             });
 
             //clear form value
@@ -65,28 +65,24 @@ const ThoughtForm = () => {
 
     //returning JSX
     return (
-
-        <div className='w-full'>
-
+       
+          <div className='w-full'>
+            
             <form className='flex flex-col items-center p-2' onSubmit={handleFormSubmit}>
                 <label html4="textarea" className="text-3xl text-white mb-4">Post:</label>
-                <textarea placeholder="Share something new!" value={thoughtText} name="textarea" className=' block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' rows='4' columns='5' onChange={handleChange}> </textarea>
+                <textarea placeholder= "Share something new!"  value={thoughtText} name="textarea" className=' block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' rows='4' columns='5' onChange={handleChange}> </textarea>
+
+                <p className= {`text-lg text-gray-500 m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+                {error && <span className= 'ml-2'>Something went wrong..</span>}
+            </p>
+        
+           
+                <button className='btn-minecraft m-2' type='submit'>Submit</button>
+             
 
 
-                <form className='content-center' onSubmit={handleFormSubmit}>
-                    <textarea placeholder="Share something new!" value={thoughtText} className='bg-white border-2 border-gray-300 shadow-lg px-3 py-2 rounded-lg focus:outline-none focus:border-gray-500' onChange={handleChange}> </textarea>
-
-                    <p className={`text-lg text-gray-500 m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
-                        {error && <span className='ml-2'>Something went wrong..</span>}
-                    </p>
-
-
-                    <button className='btn-minecraft m-2' type='submit'>Submit</button>
-
-
-
-                </form>
-        </div>
+            </form>
+            </div>
     );
 };
 
--- client/src/Components/ThoughtList/index.js
+++ client/src/Components/ThoughtList/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 //Import Icons
-import { FaRegClock, FaRegComments } from 'react-icons/fa'
+import {FaRegClock, FaRegComments }from 'react-icons/fa'
 
 const ThoughtList = ({ thoughts, title }) => {
 	if (!thoughts.length) {
@@ -10,56 +10,54 @@ const ThoughtList = ({ thoughts, title }) => {
 	}
 
 	return (
-		<div className="mx-auto items-center justify-center max-w-screen-lg rounded-xl group sm:flex  bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+		 <div className="mx-auto items-center justify-center max-w-screen-lg rounded-xl group sm:flex  bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
 			<div className="sm:w-8/12 p-5 justify-center">
 				<div className="space-y-2">
-					<div className="space-y-4 bg-gray-500">
-						<h3 className="text-xl p-3 font-bold text-center text-white">
-							{title}
+				<div className="space-y-4 bg-gray-500">
+					<h3 className="text-xl p-3 font-bold text-center text-white">
+						{title}
 						</h3>
-					</div>
-					<div className="flex-col items-center py-4 justify-between">
-						{thoughts &&
-							thoughts.map((thought) => (
-
-								<div key={thought._id} className="gap-3 space-y-1 w-auto">
-
-									<p className="flex items-center p-1 mx-auto justify-between bg-gray-500 font-bold">
-										<Link
-											to={`/profile/${thought.username}`}
-											style={{ fontWeight: 700 }}
-											className='text-light'
-										>
-											{thought.username}
-										</Link>{" "}
-										{""}
-										<span className="text-xs text-white justify-between" >
-											<FaRegClock size={14} /> {thought.createdAt}
-										</span>
-									</p>
-
-									<div >
-
-										<p>{thought.thoughtText}</p>
-
-										<Link to={`/thought/${thought._id}`}>
-											<div className='flex px-2 mb-2 items-center mx-auto justify-center'>< FaRegComments size={18} />
-
-												{thought.reactionCount} <button className="btn-minecraft">Click to {" "}
-													{thought.reactionCount ? "see" : "start"} the discussion! </button>
-											</div>
-
-										</Link>
-									</div>
-							))}
-								</div>
 				</div>
+				<div className="flex-col items-center py-4 justify-between">
+					{thoughts &&
+						thoughts.map((thought) => (
+							
+							<div key={thought._id} className="gap-3 space-y-1 w-auto">
+								
+								<p className="flex items-center p-1 mx-auto justify-between bg-gray-500 font-bold">
+									<Link
+										to={`/profile/${thought.username}`}
+										style={{ fontWeight: 700 }}
+										className='text-light'
+									>
+										{thought.username}
+									</Link>{" "}
+									{""}
+									<span className="text-xs text-white justify-between" >
+									<FaRegClock size={14} /> {thought.createdAt}
+										 </span>
+								</p>
+
+								<div >
+									
+										<p>{thought.thoughtText}</p>
+										
+										<Link to={`/thought/${thought._id}`}>
+										<div className='flex px-2 mb-2 items-center mx-auto justify-center'>< FaRegComments size={18} />    
+										
+										{thought.reactionCount} <button className="btn-minecraft">Click to {" "}
+											{thought.reactionCount ? "see" : "start"} the discussion! </button>
+											</div>
+					
+									</Link>
+								</div>
+							</div>
+						))}
+					</div>	
 				</div>
 			</div>
-			);
+		 </div>
+	);
 };
 
-			export default ThoughtList;
-
-
-
+export default ThoughtList;
