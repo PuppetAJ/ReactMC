a5aa291c579a1caf3b566a51a72dd90a05b79832
p5eudoDjnn <alexander.havers@gmail.com>
2022-12-19 19:34:52 -0500

    fixed profile style

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,93 +1,95 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom'
-import Auth from '../utils/auth'
-import ThoughtList from '../Components/ThoughtList'
-import FriendList from '../Components/FriendList'
-import ThoughtForm from '../Components/ThoughtForm'
-import { useQuery, useMutation } from '@apollo/client'
-import { QUERY_USER, QUERY_ME } from '../utils/queries'
-import { ADD_FRIEND } from '../utils/mutations'
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import Auth from "../utils/auth";
+import ThoughtList from "../Components/ThoughtList";
+import FriendList from "../Components/FriendList";
+import ThoughtForm from "../Components/ThoughtForm";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
 // import Footer from '../Components/Footer';
 
 //Import Icons
-import {AiOutlineUserAdd} from 'react-icons/ai';
-
+import { AiOutlineUserAdd } from "react-icons/ai";
 
 const Profile = () => {
+	const [addFriend] = useMutation(ADD_FRIEND);
 
-  const [addFriend] = useMutation(ADD_FRIEND)
+	const { username: userParam } = useParams();
 
-  const { username: userParam } = useParams();
+	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+		variables: { username: userParam },
+	});
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam }
-  });
+	const user = data?.me || data?.user || {};
 
-  const user = data?.me || data?.user || {};
+	if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+		return <Navigate to="/profile:username" />;
+	}
 
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/profile:username"/>
-  }
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+	const handleClick = async () => {
+		try {
+			await addFriend({
+				variables: { id: user._id },
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  const handleClick = async () => {
-    try {
-      await addFriend({
-        variables: { id: user._id }
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }
+	if (!user?.username) {
+		return (
+			<h3 className="btn-minecraft flex flex-col items-center m-auto animate-pulse">
+				You need to be logged in to see this page. Please log in or sign up!
+			</h3>
+		);
+	}
 
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this page. Please log in or sign up!
-      </h4>
-    )
-  }
+	return (
+		<main>
+			<div className=" grow items-center border-x-2 max-w-screen-lg mx-auto border-gray-800">
+				<div className='text-center rounded-lg p-2 mt-2'>
+					<h2 className='container mx-auto mb-3 text-white text-3xl p-3'>
+						Viewing {userParam ? `${user.username}'s` : "Your"} Profile
+					</h2>
+					{userParam && (
+						<div className="flex flex-col items-center text-white">
+							<button
+								className='flex mr-6 items-center duration:300 hover:scale-110'
+								onClick={handleClick}
+							>
+								<AiOutlineUserAdd size={32} />{" "}
+								<p className="mx-4 text-sm">Add friend</p>
+							</button>
+						</div>
+					)}
+				</div>
 
-  return (
-    <main>
-    <div className=" grow items-center border-x-2 max-w-screen-lg mx-auto border-gray-800">
-      <div className='text-center rounded-lg p-2 mt-2'>
-        <h2 className='container mx-auto mb-3 text-white text-3xl p-3'>
-          Viewing {userParam ? `${user.username}'s` : 'Your'} Profile
-        </h2>
-        {userParam && (
+				<div className="flex-row justify-center mb-3">
+					<div className="mt-3">
+						<ThoughtList
+							thoughts={user.thoughts}
+							title={`${user.username}'s thoughts...`}
+						/>
+					</div>
 
-          <div className="flex flex-col items-center text-white">
-          <button className='flex mr-6 items-center duration:300 hover:scale-110' onClick={handleClick}>
-           <AiOutlineUserAdd size={32} /> <p className="mx-4 text-sm">Add friend</p>
-          </button>
-
-          </div>
-        )}
-      </div>
-
-      <div className="flex-row justify-center mb-3">
-        <div className="mt-3">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
-        </div>
-
-        <div className='text-center text-lg bg-opacity-40 shadow-xl rounded-lg p-2 mt-2'>
-          <FriendList
-            username={user.username}
-            friendCount={user.friendCount}
-            friends={user.friends}
-          />
-        </div>
-        <div className= 'max-w-screen-lg mx-auto'>
-      </div>
-      <div className= 'mb-3'>{!userParam && <ThoughtForm />}</div>
-    </div>
-    </div>
-    </main>
-  );
+					<div className='text-center text-lg bg-opacity-40 shadow-xl rounded-lg p-2 mt-2'>
+						<FriendList
+							username={user.username}
+							friendCount={user.friendCount}
+							friends={user.friends}
+						/>
+					</div>
+					<div className='max-w-screen-lg mx-auto'></div>
+					<div className='mb-3'>{!userParam && <ThoughtForm />}</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Profile;
