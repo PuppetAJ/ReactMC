74512c84f8a2e8138882d218c5d731f96efb6c89
p5eudoDjnn <alexander.havers@gmail.com>
2022-12-20 21:51:31 -0500

    saved with not push

--- client/src/Pages/SingleThought.js
+++ client/src/Pages/SingleThought.js
@@ -1,69 +1,54 @@
-import React from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { useQuery } from '@apollo/client'
-import { QUERY_THOUGHT } from '../utils/queries'
-import ReactionList from '../Components/ReactionList'
-import ReactionForm from '../Components/ReactionForm'
-import Auth from '../utils/auth'
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHT } from "../utils/queries";
+import ReactionList from "../Components/ReactionList";
+import ReactionForm from "../Components/ReactionForm";
+import Auth from "../utils/auth";
 
 //Import Icons
-import { FaRegClock } from 'react-icons/fa';
+import { FaRegClock } from "react-icons/fa";
 
-const SingleThought = props => {
+const SingleThought = (props) => {
+	const { id: thoughtId } = useParams();
 
-  const { id: thoughtId } = useParams();
+	const { loading, data } = useQuery(QUERY_THOUGHT, {
+		variables: { id: thoughtId },
+	});
 
-  const { loading, data } = useQuery(QUERY_THOUGHT, {
-    variables: { id: thoughtId }
-  });
+	const thought = data?.thought || {};
 
-  const thought = data?.thought || {};
+	if (loading) {
+		return <div className="text-gray-300 text-4xl">Loading...</div>;
+	}
 
-  if (loading) {
-    return <div className="text-gray-300 text-4xl">Loading...</div>;
-  }
+	return (
+		<div className="grow max-w-screen-lg mx-auto bg-opacity-40 items-center justify-center rounded-xl w-full text-gray-300 shadow-xl hover:rounded-2xl">
+			<div className="w-full rounded-lg p-5 my-3">
+				<div className="flex items-center justify-between text-xl font-bold p-2 px-3 text-gray-300 bg-gray-700 bg-opacity-40 text-center rounded-lg ">
+					<h4 className="duration-300 hover:scale-105 hover:underline">
+						<Link to={`/profile/${thought.username}`}>
+							{thought.username}
+						</Link>{" "}
+					</h4>
+					<div className='flex items-center text-gray-400 text-xs justify-end'>
+						<FaRegClock size={14} /> <p className="pl-3">{thought.createdAt}</p>
+					</div>
+				</div>
 
-  return (
-    <div className="grow max-w-screen-lg mx-auto bg-opacity-40 items-center justify-center rounded-xl w-full text-gray-300 shadow-xl hover:rounded-2xl">
+				<div className="break-all my-2 p-2 mb-6 shadow-lg rounded-lg">
+					<p className="text-ellipsis overflow-hidden">{thought.thoughtText}</p>
+				</div>
 
-      <div className= "w-full rounded-lg p-5 my-3">
-        <div className="flex items-center justify-between text-xl font-bold p-2 px-3 text-gray-300 bg-gray-700 bg-opacity-40 text-center rounded-lg ">
-        <h4 className="duration-300 hover:scale-105 hover:underline">
-          <Link   
-          to={`/profile/${thought.username}`}>
-            {thought.username}
-          </Link>{' '}
-        </h4>
-        <div className='flex items-center text-gray-400 text-xs justify-end'>
-									<FaRegClock size={14} /> <p className="pl-3">{thought.createdAt}</p>
-										 </div>
-                     </div>
-
-
-        <div className="break-all my-2 p-2 mb-6 shadow-lg rounded-lg">
-          <p className="text-ellipsis overflow-hidden">{thought.thoughtText}</p>
-        </div>
-
-
-        
-
-        <div className="w-full">
-          {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-          {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
-         
-        </div>
-      </div>
-
-    </div>
-  )
-}
+				<div className="w-full">
+					{thought.reactionCount > 0 && (
+						<ReactionList reactions={thought.reactions} />
+					)}
+					{Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default SingleThought;
-
-
-
-
-
-
-
-
